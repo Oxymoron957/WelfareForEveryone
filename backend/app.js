2c3788e90e4a41f93587d6257b874581c9d24ea2
KWON TAE HOON <h9579895@gmail.com>
2021-07-20 16:06:06 +0900

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,17 @@ const user_router = require('./controller/user');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.json());
 
-app.use(user_router);
+// app.use(user_router);
+
+// Middleware 생성
+app.use((req, res, next)=>{
+    console.log("Request Recieved"); // Request 받았다고 출력
+    const approve ={'result':'Successful'}; // 반환할 JSON 생성
+    res.send(approve); // JSON 보내기 
+});
 
 app.listen(3000);
 
